375ae50bcc375aa2207572fa768c5dc4d1c092a1
Eduardo Neto <edu.neto@icloud.com>
2019-11-14 15:02:07 +0000

    Completed map, resized weather icon, increased size of sliders on desktop.

--- logic.js
+++ logic.js
@@ -188,94 +188,100 @@ const weatherApiInfoLoader = callback => {
 };
 
 //GOOGLE MAPS API
-var map, infoWindow;
+
 function initMap() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var myLatLng = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 52.487144, lng: -1.886977 },
+    zoom: 13
+  });
+  var card = document.getElementById("pac-card");
+  var input = document.getElementById("pac-input");
+  var types = document.getElementById("type-selector");
+  var strictBounds = document.getElementById("strict-bounds-selector");
 
-      map = new google.maps.Map(document.querySelector(".map_container"), {
-        center: myLatLng,
-        zoom: 9
-      });
-      // infoWindow = new google.maps.InfoWindow();
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
 
-      // if (navigator.geolocation) {
-      //   navigator.geolocation.getCurrentPosition(
-      //     function(position) {
-      //       var pos = {
-      //         lat: position.coords.latitude,
-      //         lng: position.coords.longitude
-      //       };
+  var autocomplete = new google.maps.places.Autocomplete(input);
 
-      //       infoWindow.setPosition(pos);
-      //       infoWindow.setContent("You are here.");
-      //       infoWindow.open(map);
-      //       map.setCenter(pos);
-      //     },
-      //     function() {
-      //       handleLocationError(true, infoWindow, map.getCenter());
-      //     }
-      //   );
-      // } else {
-      //   // Browser doesn't support Geolocation
-      //   handleLocationError(false, infoWindow, map.getCenter());
-      // }
+  // Bind the map's bounds (viewport) property to the autocomplete object,
+  // so that the autocomplete requests use the current map bounds for the
+  // bounds option in the request.
+  autocomplete.bindTo("bounds", map);
 
-      var input = document.querySelector("#postcode_input");
-      var searchBox = new google.maps.places.SearchBox(input);
+  // Set the data fields to return when the user selects a place.
+  autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
 
-      map.addListener("bounds_changed", () => {
-        searchBox.setBounds(map.getBounds());
-      });
+  var infowindow = new google.maps.InfoWindow();
+  var infowindowContent = document.getElementById("infowindow-content");
+  infowindow.setContent(infowindowContent);
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
 
-      var markers = [];
+  autocomplete.addListener("place_changed", function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
 
-      searchBox.addListener("places_changed", () => {
-        var places = searchBox.getPlaces();
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17); // Why 17? Because it looks good.
+    }
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
 
-        if (places.length === 0) return;
+    var address = "";
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] &&
+          place.address_components[0].short_name) ||
+          "",
+        (place.address_components[1] &&
+          place.address_components[1].short_name) ||
+          "",
+        (place.address_components[2] &&
+          place.address_components[2].short_name) ||
+          ""
+      ].join(" ");
+    }
 
-        markers.forEach(m => {
-          m.setMap(null);
-        });
-        markers = [];
+    infowindowContent.children["place-icon"].src = place.icon;
+    infowindowContent.children["place-name"].textContent = place.name;
+    infowindowContent.children["place-address"].textContent = address;
+    infowindow.open(map, marker);
+  });
 
-        var bounds = new google.maps.LatLngBounds();
-
-        places.forEach(p => {
-          if (!p.geometry) return;
-          markers.push(
-            new google.maps.Marker({
-              map: map,
-              title: p.name,
-              position: p.geometry.location
-            })
-          );
-          if (p.geometry.viewport) {
-            bounds.union(p.geometry.viewport);
-          } else {
-            bounds.extend(p.geometry.location);
-          }
-        });
-        map.fitBounds(bounds);
-      });
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  function setupClickListener(id, types) {
+    var radioButton = document.getElementById(id);
+    radioButton.addEventListener("click", function() {
+      autocomplete.setTypes(types);
     });
   }
-}
 
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//   infoWindow.setPosition(pos);
-//   infoWindow.setContent(
-//     browserHasGeolocation
-//       ? "Error: The Geolocation service failed."
-//       : "Error: Your browser doesn't support geolocation."
-//   );
-//   infoWindow.open(map);
-// }
+  setupClickListener("changetype-all", []);
+  setupClickListener("changetype-address", ["address"]);
+  setupClickListener("changetype-establishment", ["establishment"]);
+  setupClickListener("changetype-geocode", ["geocode"]);
+
+  document
+    .getElementById("use-strict-bounds")
+    .addEventListener("click", function() {
+      console.log("Checkbox clicked! New state=" + this.checked);
+      autocomplete.setOptions({ strictBounds: this.checked });
+    });
+}
 
 //FETCH WITH UNSPLASH API
 const backgroundRender = query => {
@@ -353,7 +359,7 @@ const app = () => {
   tasksFromList();
   modal(btnNewList, modalCreateList, closeNewListModalButton);
   modal(btnActionCreateNewTeask, modalCreateTask, closeNewTaskModal);
-  modal(btnActionFindPostcode, modalPostcode, closePostcodeModal);
+  // modal(btnActionFindPostcode, modalPostcode, closePostcodeModal);
   goBackToListsFromTasks();
   btnSubmtitAnimationToggle(btnSubmitNewList, listNameInput);
   btnSubmtitAnimationToggle(btnSubmitNewTask, taskNameInput);
